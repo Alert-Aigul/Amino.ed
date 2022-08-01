c52966a424bb24109e1351987181bb564546530d
Alert-Aigul <alertaigul@gmail.com>
2022-08-01 19:01:34 +0300

    Update docs

--- examples/chat-bot.py
+++ examples/chat-bot.py
@@ -2,20 +2,20 @@ from aminoed import *
 
 bot = Client(prefix="!")
 
-@bot.execute()
+@bot.execute() # Runs in the background, after bot.start()
 async def on_start():
     print(f"Bot {bot.auth.user.nickname} working.")
     
-@bot.command()
+@bot.command() # Comand name - function name
 async def help(event: Event):
     await event.send_message("[c]Comands\n" \
         "?chatmembers - members count.")
-
+    
 @bot.command("1000-7", "")
 async def deadinside(event: Event):
     await event.reply_message("993")
-
-@bot.command("chatmembers", "?")
+    
+@bot.command("chatmembers", "?") # Custom command and prefix
 async def chatmembers(event: Event):
     chat = await event.client.get_chat_thread(event.threadId)
     await event.send_message(f"Chat members count: {chat.membersCount}.")
